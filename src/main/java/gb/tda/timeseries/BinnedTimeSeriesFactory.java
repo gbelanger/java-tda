b8e1640390967e49cd14a69ae9699a2b685c8df0
Guillaume Belanger <gbelanger@sciops.esa.int>
2022-07-13 11:04:03 +0200

    Implemented BinnedTimeSeriesFactory

--- src/main/java/gb/tda/timeseries/BinnedTimeSeriesFactory.java
+++ src/main/java/gb/tda/timeseries/BinnedTimeSeriesFactory.java
@@ -4,8 +4,25 @@ import org.apache.log4j.Logger;
 
 public class BinnedTimeSeriesFactory {
     private static final Logger logger  = Logger.getLogger(BinnedTimeSeriesFactory.class);
-
+    
     public static BinnedTimeSeries create(BinnedTimeSeries ts) {
         return new BinnedTimeSeries(ts);
     }
+    
+    public static BinnedTimeSeries create(double[] binEdges, double[] intensities) {
+        return new BinnedTimeSeries(binEdges, intensities);
+    }
+
+    public static BinnedTimeSeries create(double tstart, double[] binEdges, double[] intensities) {
+        return new BinnedTimeSeries(tstart, binEdges, intensities);
+    }
+
+    public static BinnedTimeSeries create(double[] binEdges, double[] intensities, double[] uncertainties) {
+        return new BinnedTimeSeries(binEdges, intensities, uncertainties);
+    }
+
+    public static BinnedTimeSeries create(double tstart, double[] binEdges, double[] intensities, double[] uncertainties) {
+        return new BinnedTimeSeries(tstart, binEdges, intensities, uncertainties);
+    }
+
 }
