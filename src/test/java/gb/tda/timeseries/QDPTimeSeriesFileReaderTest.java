c21d35eaa535475c3613231fb05d74c6ad569b67
Guillaume Belanger <gbelanger@sciops.esa.int>
2022-08-02 15:33:01 +0200

    Working version of FitsTimeSeriesFileReader

--- src/test/java/gb/tda/timeseries/QDPTimeSeriesFileReaderTest.java
+++ src/test/java/gb/tda/timeseries/QDPTimeSeriesFileReaderTest.java
@@ -1,16 +1,12 @@
 package gb.tda.timeseries;
 
-public class FitsTimeSeriesFileReaderTest {
+public class QDPTimeSeriesFileReaderTest {
 
     public static void main(String[] args) throws Exception {
-        String filename1 = "saxj1808_lc1.fits";
+        String filename1 = "";
         BinnedTimeSeries ts1 = (BinnedTimeSeries) BinnedTimeSeriesFactory.create(filename1);
 
         String filename2 = "flare.fits";
         BinnedTimeSeries ts2 = (BinnedTimeSeries) BinnedTimeSeriesFactory.create(filename2);
-
-        String filename3 = "ts_isgri_255.706_-48.790_20-35keV.fits";
-        CodedMaskTimeSeries ts3 = (CodedMaskTimeSeries) BinnedTimeSeriesFactory.create(filename3);
-
     }
 }
